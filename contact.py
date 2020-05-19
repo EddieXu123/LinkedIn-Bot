f3c1221cc1b62c4dfa0289a3e919c79ac32ae29b
Eddie Xu <50087436+EddieXu123@users.noreply.github.com>
2020-05-19 03:17:16 -0400

    Update contact.py

--- contact.py
+++ contact.py
@@ -1,2 +1,5 @@
 email = "tenbotsinten@gmail.com"
 password = "waterisgood"
+company = "AthenaHealth"  # change this with whichever company you want
+search_entry = company + " recruiter"
+# Look into bot-prevention when you connect with too many people
