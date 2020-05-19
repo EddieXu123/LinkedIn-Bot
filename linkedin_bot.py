934fdef5d0eb1b18875d213482f5725414bbcff5
Eddie Xu <50087436+EddieXu123@users.noreply.github.com>
2020-05-19 02:37:05 -0400

    Update linkedin_bot.py
    Added features to connect/message

--- linkedin_bot.py
+++ linkedin_bot.py
@@ -1,21 +1,7 @@
-# Intro: I was scrolling through my LinkedIn feed and saw tons of posts of people whose internships
-# got canceled due to COVID-19, but fortunately landed another remote internship. I knew that the recruiters
-# helping these people were working extremely hard behind the scenes and wanted to acknowledge them in some way
-# As a result, I built a LinkedIn bot that would log on, search a company's recruiters, and send them a
-# note expressing gratitude for their hard work during these times. It was a short letter and only a few
-# recruiters were actually accepting connection requests/messages, but many of the ones who did replied with
-# thanks, making the whole experience worth it!
-
-
-# Plan
-# Log into my LinkedIn
-# Go to search bar, at the start of the day, put in 3 companies.
-# The bot will search company #1-3 + recruiters, connect with any recruiter by sending a message
-# If it is possible to message them, then write them the same message. close, and continue
-#
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 from pynput.keyboard import Key, Controller
 import pyautogui
 from contact import email, password, company, search_entry
@@ -31,31 +17,36 @@ class LinkedInBot:
         # Go to LinkedIn
         self.driver.get(
             'https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
-        sleep(3)
+        sleep(2)
         # Enter email
         self.driver.find_element_by_xpath('/html/body/div/main/div[2]/form/div[1]/input').send_keys(email)
-        sleep(2)
+        sleep(1)
         # Enter password
         self.driver.find_element_by_xpath('/html/body/div/main/div[2]/form/div[2]/input').send_keys(password + Keys.RETURN)
-        sleep(5)
+        sleep(8)
 
     def daily_post(self):
-        start_post = self.driver.find_element_by_xpath('/html/body/div[7]/div[3]/div/div/div/div/div[1]/div/div[1]/button[1]')
+        start_post = self.driver.find_element_by_class_name('share-box-feed-entry__trigger')
         # Click on Start a Past
         start_post.click()
+        sleep(1)
+        # Whatever you want to post on your profile
+        message = "Hi everyone! I recently got a LinkedIn, please connect with me!"
         # Select text to enter post
         self.driver.find_element_by_xpath(
             '/html/body/div[4]/div/div/div[2]/div/div[1]/div[1]/div[2]/div/div/div/div/div[1]/p').send_keys(message)
         # Post the message
-        self.driver.find_element_by_xpath('//*[@id="ember462"]').click()
+        sleep(2)
+        self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]/div/div[2]/div[2]/button').click()
+        sleep(3)
 
     def like_other_posts(self):
         # post is the array that stores every single 'like button' in your feed
         # which you can then click as you go on
-        post = bot.driver.find_elements_by_class_name('react-button__text')
+        post = self.driver.find_elements_by_class_name('react-button__text')
         # If the post has been liked already, it will have in the outerHTML:"like"
         has_been_liked = "like"
-        i = 0 # Starting with the first post in your feed
+        i = 0  # Starting with the first post in your feed
         while i < 10:  # replace 10 with how ever many posts you want to like in your feed
             # If the post you are on has NOT been liked already,
             if not has_been_liked in post[i].get_attribute('outerHTML'):
@@ -65,105 +56,101 @@ class LinkedInBot:
                 # Posts that are further down in your feed are hidden from the driver.
                 # To access them, every time you like a post (which causes the driver to scroll
                 # down more into your feed), reset the 'post' array with all the possible elements
-                post = bot.driver.find_elements_by_class_name('react-button__text')
+                post = self.driver.find_elements_by_class_name('react-button__text')
                 i += 1
                 sleep(1)
             else:
                 # Otherwise, keep scrolling down as if we had liked it to get more elements
                 i += 1
                 sleep(1)
+        sleep(4)
 
-    def connect_and_message(self):
+    def search_company(self):
         # Go to search bar and enter your company name
         search = self.driver.find_element_by_xpath('/html/body/header/div/form/div/div/div/div/div[1]/div[1]/input')
         search.click()
         search.send_keys(search_entry + Keys.RETURN)
+        sleep(4)
 
     def click_connect(self):
-        # Sometimes, you will get company results for what you have searched,
-        # in the form of 'cards' you can 'follow'. These have the same name as the
-        # 'connect' button. However, they also have an additional "mb2" to distinguish them
-        # from an actual person. So, to ignore these, just choose the buttons without "mb2"
-
-        # variable to store all the 'connects' (and cards) on a page
-        connect = self.driver.find_elements_by_class_name('search-result__action-button')
-        message = self.driver.find_elements_by_class_name('message-anywhere-button')
+        # Sometimes, you will get company results for what you have searched in the form of 'cards' you can 'follow'.
+        # These cards have the same class_name as the 'connect' button. However, they also have an additional
+        # "Follow/Unfollow" attribute in their outerHTML to distinguish them from an actual person.
+        # So, to ignore these, just choose the buttons without "ollow"
 
         page_count = 0
         while page_count < 5:  # Connect/Message recruiters on as many pages as you want
+            # variable to store all the 'connect' buttons (and cards) on a page
+            connect = self.driver.find_elements_by_class_name('search-result__action-button')
+            # variable to store all the 'message' buttons on a page
+            message = self.driver.find_elements_by_class_name('message-anywhere-button')
+
             for i in range(0, len(connect)):  # For every connect/follow button on the page
-                if not "mb2" in connect[i]:  # If the button I am on is not a 'card'
+                connect = self.driver.find_elements_by_class_name('search-result__action-button')  # Need to reset connect counter
+                if not "ollow" in connect[i].get_attribute('outerHTML'):  # If the button I am on is not a 'card'
                     connect[i].click()  # connect with the person
+                    sleep(2)
+
+                    # We could be at a place where we've already connected with this person, so see if
+                    # we get the same popup after clicking 'connect'
+                    try:
+                        self.driver.find_element_by_class_name('flex-1').get_attribute('innerHTML')
+                    except NoSuchElementException:
+                        continue
+                    # When you connect with someone, a popup appears, saying your invitation is on the way
+                    # The class_name for this text is flex-1 and the innerHTML is 'your invite to ____ is on the way...'
+                    # invite_message holds the innerHTML of little paragraph that pops up when you send a connection
+                    invite_message = self.driver.find_element_by_class_name('flex-1').get_attribute('innerHTML')
+                    name_of_connection = ""  # Set an empty name variable we will append the connections name into
+                    # Their name starts at the 44th index position of the innerHTML and ends when there is a space (to denote first name)
+                    # or when there is a '<' (if you want their full name: '<' denotes the end of the 'bold' HTML tag their name is in)
+                    for character in range(44, len(invite_message)):
+                        if not ' ' in invite_message[character]:
+                            name_of_connection += invite_message[character]
+                        else:  # once we reach the space (or closing HTML tag), we have the person's first/full name so we can break
+                            break
+                    # Now, name_of_connection gives you the name of the person you sent a connection to
+
+                    # Now, we can add a nice note :)
+
+                    # mr1 is one of 3 buttons that we can click when we get the 'your invite is on the way' popup
+                    add_note = self.driver.find_elements_by_class_name('mr1')
+                    add_note[1].click()  # The second webElement is the one that clicks 'Add a note'
+
+                    # Write your note here (Note I have already set up a template with "Hi 'name of person'!")
+                    connect_message = "I was scrolling through LinkedIn and just wanted to tell you that you are amazing! " \
+                                      "I know your job is tough but what you do genuinely helps so many people and I just wanted to recognize that! " \
+                                      "Keep up the great work and have an amazing day!"
+                    connect_invite = "Hi " + name_of_connection + "! " + connect_message
+                    self.driver.find_element_by_xpath('//*[@id="custom-message"]').send_keys(connect_invite)
+                    done_with_message = self.driver.find_elements_by_class_name('artdeco-button__text')
+                    for button in range(0, len(done_with_message)):
+                        if 'Done' in done_with_message[i].get_attribute('innerHTML'):  # my button is the 'Done' button
+                            done_with_message[i].click()
+                            break
+
+                    # Now, I have sent the message to my connection and can keep going
+                    sleep(3)
             # Now, I have connected with everyone on the page
             # Time to send messages to those who allow me to do so!
             for j in range(0, len(message)):
                 message[j].click()
-                compose_message = bot.driver.find_element_by_class_name('ember-text-field')
+                compose_message = self.driver.find_element_by_class_name('ember-text-field')
                 subject = "Thank you!"  # Change to whatever you want!
                 message_body = "Hi! I was scrolling through LinkedIn and just wanted to tell you" \
-                               "that you are amazing! Keep up the good work - your efforts " \
-                               " to thank you for all the amazing work you've been" \
-                               "doing lately! It definitely ! Have an amazing day! :)" \
+                               "that you are amazing! I know your job is tough but what you do genuinely helps so many people" \
+                               "and I just wanted to recognize that! Keep up the great work and have an amazing day!"
                 compose_message.send_keys(subject + Key.tab + message_body)
-
-
-
-
-
+                # Send the message
+                self.driver.find_elements_by_class_name('msg-form__send-button').click()
         # Go to next page
         driver.find_element_by_xpath('//*[@id="ember7512"]/span').click()
+        page_count += 1
 
-
-"""
 # Calling the functions to run the program
-bot = YouTubeBot()
-bot.log_on_and_like()
-"""
-
-
-#bot = LinkedInBot()
-#bot.message_recruiter()
-
-#finite state machine check if I finished everyone first
-
-
-# inner HTML have button
-# if inner HTML has a button, click that button
-# member = recruiter[i].get_attribute('innerHTML')
-# if 'button' in member:
-#     click.button
-# matches[i].get_attribute('innerHTML')
-# Scroll down 2 times, so hit space bar 2 times on keyboard to get 10 entities
-#self.driver.find_elements_by_class_name('search-entity') Gives all people on screen
-#search-results__list
-# Also need to take care of when you send too many connections and need to enter email
-
-
-'/html/body/div[7]/div[3]/div/div/div/div/div[3]/div[2]/div/div/div[5]/div/div[2]/span[1]/button[1]/span/span'
-
-
-# Button to go to next page
-'/html/body/div[8]/div[4]/div/div[2]/div/div[2]/div/div/div/div/div[3]/div/button[2]'
-
-
-post = bot.driver.find_elements_by_class_name('react-button__text')
-
-'//*[@id="ember7512"]/span'
-'//*[@id="ember7512"]/span'
-
-
-# If they have mb2, then they are a card, not a person
-# Connect
-search-result__action-button
-# Message
-message-anywhere-button
-
-
-
- visually-hidden
-'//*[@id="a11y-ember790"]'
-
-
-text = bot.driver.find_element_by_class_name('ember-text-field')
-msg-form__contenteditable t-14 t-black--light t-normal flex-grow-1 notranslate
-contenteditable
+bot = LinkedInBot()
+bot.log_on()
+bot.daily_post()
+bot.like_other_posts()
+bot.search_company()
+bot.click_connect()
